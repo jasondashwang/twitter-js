e190a37f04c82bab857e220455efd97f1f2ae93a
Jason Wang <jasonwang67@gmail.com>
2016-06-14 13:04:21 -0400

    finished configuring express

---
+++ app.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var app = new express();
+
+app.use(function(req, res, next){
+  console.log(req.method, req.path);
+  next();
+});
+
+app.get('/', function(req, res, next){
+  console.log('you reached the root!');
+  next();
+})
+
+app.get('/special/:directory', function(req, res, next){
+  console.log('you reached the special area');
+  throw Error('bad area!');
+  next();
+});
+
+app.use(function(req, res, next){
+  console.log(req.method, req.path, 200);
+  res.end();
+});
+
+app.use(function(err, req, res, next){
+  console.log(req.method, req.path, 400);
+
+  res.end();
+})
+
+app.listen(3000);
+
