88fb1bcce89772963b45d8a9e00a0369e6e3c6af
Soh Nagano <soh.nagano@gmail.com>
2016-06-14 16:38:05 -0400

    tweet form added

--- routes/index.js
+++ routes/index.js
@@ -5,24 +5,24 @@ var tweetBank = require('../tweetBank');
 
 router.get('/', function (req, res) {
   var tweets = tweetBank.list();
-  res.render( 'index', { title: 'Twitter.js', tweets: tweets } );
+  res.render( 'index', { title: 'Twitter.js', tweets: tweets, showForm: true} );
 });
 
 router.get('/tweets/:tweetnumber', function(req, res, next){
   var tweets = tweetBank.list();
-  res.render('index', { title: 'Tweet number ' + req.params.tweetnumber , tweets: [tweets[+(req.params.tweetnumber)]] });
+  res.render('index', { title: 'Tweet number ' + req.params.tweetnumber , tweets: [tweets[+(req.params.tweetnumber)]], showForm: false});
 });
 
 router.get( '/users/:name', function(req, res, next){
   var tweets = tweetBank.find(function(object){
   	return object.name === req.params.name;
   });
-  res.render('index', { title: 'All tweets by ' + req.params.name, tweets: tweets});
+  res.render('index', { title: 'All tweets by ' + req.params.name, tweets: tweets, showForm: false});
 });
 
 router.get('/tweets/', function(req, res, next){
   var tweets = tweetBank.list();
-  res.render( 'index', { title: 'All tweets', tweets: tweets } );
+  res.render( 'index', { title: 'All tweets', tweets: tweets, showForm: false} );
 });
 
 module.exports = router;
